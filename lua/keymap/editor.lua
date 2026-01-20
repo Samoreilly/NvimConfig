f84600a3742f2e79e75f4807bd0b8526c0404a13
Samoreilly <x23756575@student.ncirl.ie>
2026-01-20 15:44:44 +0000

    c

--- lua/keymap/editor.lua
+++ lua/keymap/editor.lua
@@ -42,6 +42,7 @@ local mappings = {
 		-- Builtins: "Suckless" - named after r/suckless
 		["n|Y"] = map_cmd("y$"):with_desc("edit: Yank text to EOL"),
 		["n|D"] = map_cmd("d$"):with_desc("edit: Delete text to EOL"),
+		["n|I"] = map_cmd("i"):with_noremap():with_desc("edit: Insert at cursor (override default)"),
 		["n|n"] = map_cmd("nzzzv"):with_noremap():with_desc("edit: Next search result"),
 		["n|N"] = map_cmd("Nzzzv"):with_noremap():with_desc("edit: Prev search result"),
 		["n|J"] = map_cmd("mzJ`z"):with_noremap():with_desc("edit: Join next line"),
